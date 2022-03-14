ae06faebc7432353be7610c877f08cd3bb220b22
KWON WONHYUN <mywonhyuni@gmail.com>
2022-03-14 11:41:52 +0900

    220314_local_delete

--- README.md
+++
@@ -1,4 +0,0 @@
-# bonocoin_daemon_aws
-bonocoin_daemon_file_at_aws
-
-220313 test from aws
--- bonocoin_win_bin/bonod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "bonod"
-echo $(
-wine ./bin/bonocoind.exe -printtoconsole -conf=$PWD -datadir=$PWD/data
-)
